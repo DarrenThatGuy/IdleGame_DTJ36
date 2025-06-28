ff6c271a8f353c08737278f24b2a9403bb58b883
Jkokol2000 <jameskokol2000@gmail.com>
2025-06-28 12:57:22 -0600

    Added Regen for boss, party attacks

--- Scripts/BossCore.gd
+++ Scripts/BossCore.gd
@@ -3,11 +3,13 @@ class_name Boss extends Node2D
 @export var max_health: float
 var health: float
 @export var regen_amount: float
+@export var regen_speed: float
 
 @export var regen_timer: Timer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	health = max_health
+	regen_timer.wait_time = regen_speed
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,8 +18,20 @@ func _process(delta):
 
 func level_up():
 	max_health *= 2
+	regen_amount = round(regen_amount * 1.5)
 	health = max_health
 
 
 func _on_regen_timer_timeout():
-	health += regen_amount
+	if health < max_health:
+		if health + regen_amount > max_health:
+			health = max_health
+		else:	
+			health += regen_amount
+	print(health)
+
+
+func _on_base_character_deal_damage(damage):
+	health -= damage
+	print(health)
+
--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -1,1 +1,19 @@
-extends Node
+class_name Character extends Node
+
+@export var attack_damage: float
+@export var attack_speed: float
+
+@export var attack_timer: Timer
+
+signal deal_damage(damage)
+
+func _ready():
+	attack_timer.wait_time = attack_speed
+	attack_timer.start()
+
+func attack():
+	print( name + " ATTACKING")
+	deal_damage.emit(attack_damage)
+	
+func _on_attack_timer_timeout():
+	attack()
